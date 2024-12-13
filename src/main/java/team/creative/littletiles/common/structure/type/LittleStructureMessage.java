b328ee9e7817094eb5425ef251156c8f73fc6f0a
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-12-13 13:42:16 +0100

    Added option to change message length allowed (default is 4098) Added option to message structure to display message in status bar Added support for minecraft's formatting (used for titles or custom message) Fixed message gui awkward layout

--- src/main/java/team/creative/littletiles/common/structure/type/LittleStructureMessage.java
+++ src/main/java/team/creative/littletiles/common/structure/type/LittleStructureMessage.java
@@ -1,15 +1,22 @@
 package team.creative.littletiles.common.structure.type;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.mojang.brigadier.StringReader;
+
+import net.minecraft.commands.ParserUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.phys.BlockHitResult;
+import team.creative.littletiles.LittleTiles;
 import team.creative.littletiles.common.block.little.tile.LittleTileContext;
 import team.creative.littletiles.common.block.little.tile.parent.IStructureParentCollection;
 import team.creative.littletiles.common.structure.LittleStructure;
@@ -20,6 +27,7 @@ public class LittleStructureMessage extends LittleStructure {
     
     public String text;
     public boolean allowRightClick = true;
+    public boolean status = false;
     
     public LittleStructureMessage(LittleStructureType type, IStructureParentCollection mainBlock) {
         super(type, mainBlock);
@@ -33,7 +41,8 @@ public class LittleStructureMessage extends LittleStructure {
     @Override
     public InteractionResult use(Level level, LittleTileContext context, BlockPos pos, Player player, BlockHitResult result) {
         if (allowRightClick) {
-            player.displayClientMessage(Component.literal(text), true);
+            if (!level.isClientSide)
+                player.displayClientMessage(message(), status);
             return InteractionResult.SUCCESS;
         }
         return super.use(level, context, pos, player, result);
@@ -41,14 +50,24 @@ public class LittleStructureMessage extends LittleStructure {
     
     @Override
     protected void loadExtra(CompoundTag nbt, HolderLookup.Provider provider) {
-        text = nbt.getString("text");
+        text = StringUtils.truncate(nbt.getString("text"), LittleTiles.CONFIG.general.messageStructureLength);
         allowRightClick = nbt.getBoolean("right");
+        status = nbt.getBoolean("status");
     }
     
     @Override
     protected void saveExtra(CompoundTag nbt, HolderLookup.Provider provider) {
         nbt.putString("text", text);
         nbt.putBoolean("right", allowRightClick);
+        nbt.putBoolean("status", status);
+    }
+    
+    public Component message() {
+        try {
+            return ParserUtils.parseJson(getStructureLevel().registryAccess(), new StringReader(text), ComponentSerialization.CODEC);
+        } catch (Exception exception) {
+            return Component.literal(text);
+        }
     }
     
     @Override
@@ -60,7 +79,7 @@ public class LittleStructureMessage extends LittleStructure {
             
             final LevelChunk chunk = level.getChunkAt(getStructurePos());
             if (chunk != null)
-                ((ServerChunkCache) chunk.getLevel().getChunkSource()).chunkMap.getPlayers(chunk.getPos(), false).forEach(x -> x.sendSystemMessage(Component.literal(text)));
+                ((ServerChunkCache) chunk.getLevel().getChunkSource()).chunkMap.getPlayers(chunk.getPos(), false).forEach(x -> x.sendSystemMessage(message(), status));
         }
     }
     
